1a725622c420649d3c23558cbb93ee593094e5e2
Marco Crupi <marcocrupi@hotmail.it>
2022-08-13 18:42:59 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6KcqgiSGw6VzlEbph8kjZA53p28f5MJc",
+  authDomain: "clone-514e2.firebaseapp.com",
+  projectId: "clone-514e2",
+  storageBucket: "clone-514e2.appspot.com",
+  messagingSenderId: "258346827525",
+  appId: "1:258346827525:web:dbc97a2864a6a0f2a8c16c",
+};
