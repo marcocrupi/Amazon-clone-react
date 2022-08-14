2b826c06bc55693e1e9291722ba2fb7a2a430606
Marco Crupi <marcocrupi@hotmail.it>
2022-08-14 15:59:14 +0200

    update header.css

--- src/Header.js
+++ src/Header.js
@@ -13,6 +13,23 @@ function Header() {
         <input className="header__searchInput" type="text" />
         {/* Logo */}
       </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
     </div>
   );
 }
