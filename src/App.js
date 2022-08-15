ff2621613ecf676ffd0f77f6a69114fdf64549fc
Marco Crupi <marcocrupi@hotmail.it>
2022-08-15 11:32:37 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <h1>I AM A CHECKOUT</h1>
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
