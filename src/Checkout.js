265783b0a0de257a38c123c2a06382dc5fbae711
Marco Crupi <marcocrupi@hotmail.it>
2022-08-15 15:04:27 +0200

    add subtotal

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AmazonAd from "./images/amazon_ad_.jpg";
+import Subtotal from "./Subtotal";
 import "./Checkout.css";
 
 function Checkout() {
@@ -21,7 +22,7 @@ function Checkout() {
       </div>
 
       <div className="checkout__right">
-        <h2>The subtotal will go here</h2>
+        <Subtotal />
       </div>
     </div>
   );
---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subtotal.css"
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <div></div>
+    </div>
+  );
+}
+
+export default Subtotal;
