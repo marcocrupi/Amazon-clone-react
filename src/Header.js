68d5c9bbda11ec5887e25550aa10d8e3a6140479
Marco Crupi <marcocrupi@hotmail.it>
2022-08-15 12:49:18 +0200

    add link home basket

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,16 @@ import React from "react";
 import AmazonLogo from "./images/amazon-logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src={AmazonLogo} alt="amazon logo" />
+      <Link to="/">
+        <img className="header__logo" src={AmazonLogo} alt="amazon logo" />
+      </Link>
+
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -29,10 +33,12 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon className="" />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon className="" />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
