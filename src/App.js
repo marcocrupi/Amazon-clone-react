b8035afbc0002bf271c53e09365a6bc9d40722ac
Marco Crupi <marcocrupi@hotmail.it>
2022-08-16 07:51:00 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             }
           />
           <Route
-            path="/"
+            path="/Amazon-clone-react"
             element={
               <>
                 <Home />
