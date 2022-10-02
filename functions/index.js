c382a24b3248565e3739017a654a0a4902fb3eb1
Marco Crupi <marcocrupi@hotmail.it>
2022-10-02 08:12:09 +0200

    update functions(back-end) and axios.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,40 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { AodOutlined } = require("@mui/icons-material");
+const stripe = require("stripe")(
+  "sk_test_51Lo6EJCT83zqzJbQNY4UwnNk6HfS5FkiEk2V9iuWqmyfFOrh7YTZtyBjpk8X4hAeFkvXLCJHLXMtC7CHQG8K6mf50065QXxsVV"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App Config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get("/", (request, response) => response.status(200).send("Hello world"));
+
+// Look at Payment.js
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+    console.log("Payment request recieved", total);
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, // subunits of the currency
+        currency: "usd",
+    });
+
+    // OK - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/clone-514e2/us-central1/api
--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "...",
+  baseURL: "http://localhost:5001/clone-514e2/us-central1/api",
 });
 
 export default instance;
